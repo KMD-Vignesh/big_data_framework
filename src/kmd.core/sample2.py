01a7de8bea9c44762e8cfeaeccefc71901d43593
KMD-Vignesh <KMD.Vignesh@outlook.com>
2025-04-09 00:34:06 +0530

    omot2

--- src/kmd.core/sample2.py
+++ src/kmd.core/sample2.py
@@ -1,19 +1,30 @@
 import pathlib
+import re
+import shutil
 import dask
 import polars as pl
 from dask import delayed
 import time
 
 
-
-def read_csv_to_parquet(csv_dir:pathlib.Path, parquet_dir:pathlib.Path) -> None:
-    parquet_dir.mkdir(parents=True, exist_ok=True)
+def read_csv_to_parquet(csv_dir: pathlib.Path, parquet_dir: pathlib.Path) -> None:
+    shutil.rmtree(parquet_dir, ignore_errors=True)
+    pattern = r'(.+)_(\d{8})\.csv$'
+    
     @delayed
-    def write_csv_to_parquet(csv_file:pathlib.Path) -> None:
-        df: pl.DataFrame = pl.scan_csv(csv_file).collect()
-        output_file: pathlib.Path = parquet_dir / (csv_file.stem + ".parquet")
-        df.write_parquet(output_file)
-    conversion_list = [write_csv_to_parquet(csv_file) for csv_file in csv_dir.glob("*.csv")]
+    def write_csv_to_parquet(csv_file: pathlib.Path) -> None:
+        match = re.match(pattern=pattern, string=csv_file.name)
+        if match:
+            base_name, date_str = match.groups()
+            output_dir: pathlib.Path = parquet_dir / f"{base_name}.delta" / f"date={date_str}"
+            output_dir.mkdir(parents=True, exist_ok=True)
+            output_file: pathlib.Path = output_dir / f"{base_name}.parquet"
+            pl.scan_csv(csv_file).collect().write_parquet(output_file)
+        else:
+            print(f"Skipping file: {csv_file.name} - Filename does not match pattern")
+    
+    print(list(csv_dir.glob("*.csv")))
+    conversion_list = [write_csv_to_parquet(csv_file) for csv_file in csv_dir.glob("**/*.csv")]
     dask.compute(*conversion_list)
 
 
@@ -24,6 +35,7 @@ def main() -> None:
     read_csv_to_parquet(csv_dir, parquet_dir)
     end: float = time.time()
     print(f"Time taken: {end - start:.2f} seconds")
+
 
 if __name__ == "__main__":
     main()
